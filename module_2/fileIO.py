8e62e3f661a5587732373d6a3e5c5fc6440c1d00
Camden McKay <cam.mckay9@gmail.com>
2024-03-11 18:40:29 -0500

    refactored fileIO

--- module_2/fileIO.py
+++ module_2/fileIO.py
@@ -1,27 +1,54 @@
+# SOLID IMPLEMENTATION: Interface segregation principle
+
+# FileIO interface is used by initializing with the filename and then using either the get_line_list
+# method to read lines from the file or the write data method to save a string to the file
+
 import os
+from abc import ABC, abstractmethod
 
-## returns list of each line inside file
-def get_line_list(filename: str) -> list[str]:
-    try:
-        file = open(filename, "r")
-        lines = list()
-        for line in file:
-            lines.append(line.strip())
+## interface for FileIO operations
+class FileIOInterface(ABC):
+    @abstractmethod
+    def __init__(self, filename: str):
+        pass
+
+    @abstractmethod
+    def get_line_list(self) -> list[str]:
+        pass
+
+    @abstractmethod
+    def write_data(self, data: str):
+        pass
+
+## implements FileIOInterface
+class FileIO(FileIOInterface):
+    def __init__(self, filename: str):
+        self.filename = filename
+    
+    ## returns list of each line inside file
+    def get_line_list(self) -> list[str]:
+        try:
+            file = open(self.filename, "r")
+            lines = list()
+            for line in file:
+                lines.append(line.strip())
+            file.close()
+            return lines
+        except IOError:
+            print(f"Failed to open {self.filename}")
+            return list()
+    
+    ## creates/overwrites file with provided data
+    def write_data(self, data: str) -> None:
+        os.makedirs(os.path.dirname(self.filename), exist_ok=True)
+        file = open(self.filename, "w")
+        file.write(data)
         file.close()
-        return lines
-    except IOError:
-        print(f"Failed to open {filename}")
-        return list()
-
-## creates/overwrites file with provided data
-def write_file(filename: str, data: str) -> None:
-    os.makedirs(os.path.dirname(filename), exist_ok=True)
-    file = open(filename, "w")
-    file.write(data)
-    file.close()
 
 def main():
-    write_file("output.log",str(get_line_list("url.in")))
+    input_file = FileIO("url.in")
+    output_file = FileIO("output.log")
+    output_file.write_data(str(input_file.get_line_list()))
 
 if __name__=="__main__":
     main()
--- run.py
+++ run.py
@@ -1,5 +1,9 @@
+# run this file from the command line
+# use --in or -i to specify input file of URLs to scrape (REQUIRED)
+# use --out or -o to specify directory to output articles to (Optional, Default: ./Data/processed)
+
 from module_1.scraper import CNNScraper
-from module_2 import fileIO
+from module_2.fileIO import FileIO
 import argparse
 
 def main():
@@ -8,14 +12,16 @@ def main():
     parser.add_argument("--out", "-o", dest="file_out", default="Data/processed", type=str, help="Directory to save scraped articles in.", required=False) # Defaults to the project defined output location
     args = parser.parse_args()
 
-    urls = fileIO.get_line_list(args.file_in)
+    input_file = FileIO(args.file_in)
+    urls = input_file.get_line_list()
     for url in urls:
         scraper = CNNScraper(url) #This uses the CNN scraper. You can implement the Scraper interface with another class and change just this line for another news site
         title = scraper.get_title()
         article = scraper.get_article()
         filename = args.file_out + "/" + title.replace(" ", "_") + ".txt"
-        if filename != args.file_out + "/.txt":
-            fileIO.write_file(filename, article)
+        if filename != args.file_out + "/.txt": # skips saving if article title ends up blank
+            output_file = FileIO(filename)
+            output_file.write_data(article)
 
 if __name__=="__main__":
     main()
